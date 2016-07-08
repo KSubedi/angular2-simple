d0bd043402579c19b97a50daf7b81a990e3846be
Kaushal Subedi <kaushal@wireshock.com>
2016-07-08 17:07:30 -0600

    added port option

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ Simple static file server that will send index.html if the file is not found. Us
 var express = require("express");
 var app = express();
 
+var PORT = 3000;
+
 app.use(express.static("dist"));
 
 app.use((req, res) => {
     res.sendFile(__dirname + "/dist/index.html");
 })
 
-app.listen(3000, function () {
-  console.log('Angular App Is Running!');
-  console.log('Open http://localhost:3000/ on your browser!');
+app.listen(PORT, function () {
+  console.log("Angular App Is Running on port " + PORT + "!");
+  console.log("Open http://localhost:" + PORT + "/ on your browser!");
 });
